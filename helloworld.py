49fe3a0ae8f483e6cabb2cd77d6b8b17cde5d028
Adela <126213057+adelacervantes-pvcc@users.noreply.github.com>
2023-12-09 22:27:14 -0500

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+# Name: Adela Cervantes
+# Prog Purpose: Hello World
+
+print("Hello World! This is my first Python Program.")
+print("I can Program in HTML, which is awesome")
+print("However, HTML cannot do arithmetic.")
+print("But python can: \n\t ADD \n\t Subtract\n\t Multiply \n\t Divide")
+print("and lots of other things!")
